667b561baccf991a83d13ffd9d6574376a73eb33
Marvanparsh <panwarpawan507@gmail.com>
2025-03-05 15:10:46 +0530

    pushed

---
+++ src/main/java/com/example/GreetingApp/service/IAuthenticationService.java
@@ -0,0 +1,11 @@
+package com.example.GreetingApp.service;
+import com.example.GreetingApp.DTO.AuthUserDTO;
+import com.example.GreetingApp.DTO.LoginDTO;
+import com.example.GreetingApp.model.AuthUser;
+
+
+public interface IAuthenticationService {
+    AuthUser register(AuthUserDTO userDTO) throws Exception;
+
+    String login(LoginDTO loginDTO);
+}
