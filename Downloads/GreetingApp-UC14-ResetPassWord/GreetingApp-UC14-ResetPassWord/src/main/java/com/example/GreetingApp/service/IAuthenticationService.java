052ef9f31ff12352f45566a3f85e94e225a1e441
Marvanparsh <panwarpawan507@gmail.com>
2025-03-09 00:44:00 +0530

    Pushed resetpasswd

---
+++ Downloads/GreetingApp-UC14-ResetPassWord/GreetingApp-UC14-ResetPassWord/src/main/java/com/example/GreetingApp/service/IAuthenticationService.java
@@ -0,0 +1,13 @@
+package com.example.GreetingApp.service;
+import com.example.GreetingApp.DTO.AuthUserDTO;
+import com.example.GreetingApp.DTO.LoginDTO;
+import com.example.GreetingApp.model.AuthUser;
+
+
+public interface IAuthenticationService {
+    AuthUser register(AuthUserDTO userDTO) throws Exception;
+
+    String login(LoginDTO loginDTO);
+    String forgotPassword(String email, String newPassword);
+    String resetPassword(String email, String currentPassword, String newPassword);
+}
